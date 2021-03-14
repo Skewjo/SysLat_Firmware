a2ba23ec1e4c88d23e97d5e67d89593efa45cc3f
gratefuldad <36430540+gratefuldad@users.noreply.github.com>
2021-03-14 14:02:23 -0500

    New branch to usPrecision
    New branch to play with timing

--- syslat_fw_pcb_rev_1/syslat_fw_pcb_rev_1.ino
+++ syslat_fw_pcb_rev_1/syslat_fw_pcb_rev_1.ino
@@ -8,6 +8,7 @@ int timeoutCounter = 0;
 int index = 0;
 const byte IOCpin = 7;
 bool button_flag = false;
+bool timer3_flag = false;
 
 const int shortDelay = 10;
 const int midDelay = 10;
@@ -42,6 +43,20 @@ void setup() {
   pinMode(IOCpin, INPUT);
  
   cli(); //disallow interupts while we set one up...
+
+  //set timer3 interrupt at 1Hz
+  TCCR3A = 0;// set entire TCCR1A register to 0
+  TCCR3B = 0;// same for TCCR1B
+  TCNT3  = 0;//initialize counter value to 0
+  // set compare match register for 1hz increments
+  OCR3A = 7812;   //7812 should be every 1/2 second; // = (16*10^6) / (1*1024) - 1 (must be <65536)
+  // turn on CTC mode
+  TCCR3B |= (1 << WGM12);
+  // Set CS10 and CS12 bits for 1024 prescaler
+  TCCR3B |= (1 << CS12) | (1 << CS10);  
+  // enable timer compare interrupt
+  TIMSK3 |= (1 << OCIE1A);
+  
   attachInterrupt(digitalPinToInterrupt(IOCpin), IOC, RISING);
   sei();//allow interrupts
 }
@@ -51,38 +66,53 @@ void setup() {
 //-----------------------------------------------------------BEGIN MAIN-----------------------------------------------------------//
 void loop() {
   while (timeoutCounter < 3) {
-    int i = timeTheFlash();
-    
-    lcd.setCursor(0, 0);
-
-    if (timerTotal <= 1) {
-      timeoutCounter++;
+    if(timer3_flag){
+      int i = timeTheFlash();
+      
       lcd.setCursor(0, 0);
-      lcd.print("Too Fast");
+  
+      if (timerTotal <= 1) {
+        timeoutCounter++;
+        lcd.setCursor(0, 0);
+        lcd.print("Too Fast");
+      }
+      else if (timerTotal < 500000){
+        f_timerTotal = static_cast<float>(timerTotal)/1000;
+        lcd.clear();
+        lcd.print(f_timerTotal);
+        timeoutCounter = 0;
+      }
+      if (timerTotal >= 500000) {
+        timeoutCounter++;
+        lcd.clear();
+        lcd.print("Timeout");
+        delay(10);
+        //Serial.flush();
+      }
+  
+      sendB();
+      sendData(f_timerTotal);
+      button_flag = false;
+      
+//  if(timer3_flag){
+//      lcd.clear();
+//      lcd.write("2 Hz int");
+      timer3_flag = false;
     }
-    else if (timerTotal < 500000){
-      f_timerTotal = static_cast<float>(timerTotal)/1000;
-      lcd.clear();
-      lcd.print(f_timerTotal);
-      timeoutCounter = 0;
-    }
-    if (timerTotal >= 500000) {
-      timeoutCounter++;
-      lcd.setCursor(0, 0);
-      lcd.print("Timeout");
-      delay(10);
-      Serial.flush();
-    }
-    
-    sendB();
-    sendData(f_timerTotal);
-    button_flag = false;
   }
   
   if(timeoutCounter >= 3){
-    lcd.clear();
-    lcd.write("Timeout");
-    delay(longDelay);
+    if(!Serial){
+      lcd.clear();
+      lcd.write("Syslat"); delay(longDelay);
+      lcd.clear();
+      lcd.write("disconn"); delay(longDelay);
+    }
+    else{
+      lcd.clear();
+      lcd.write("Timeout3");
+      delay(longDelay);
+    }
   }
   
   //Reset
@@ -106,10 +136,10 @@ int timeTheFlash(){
 }
 
 void sendB(){ //possibly move this first flush to directly after "timerTotal" calculation so that we can possibly get rid of the first delay?
-    Serial.flush();
-    delay(midDelay);
+    //Serial.flush();
+    //delay(midDelay);
     Serial.write("B");
-    delay(longDelay);
+    //delay(longDelay);
     Serial.flush();
 }
 
@@ -125,16 +155,21 @@ void sendData(unsigned long timerTotal){
     factor = factor/10;
     Serial.print(timerTotal/factor);
     Serial.flush();
-    delay(midDelay);
+    //delay(midDelay);
     timerTotal = timerTotal % factor;
   }
-  delay(midDelay);
+  //delay(midDelay);
   Serial.write("C");
-  delay(longDelay);
+  //delay(longDelay);
   Serial.flush();
 }
 
 void IOC()
 {
   button_flag = true; // Interrupt On Change ISR - as soon as pin goes high, this flag is set
+}
+
+ISR(TIMER3_COMPA_vect){               //timer3 interrupt 
+  //Serial.print("1Hz\r\n");      //it works!
+  timer3_flag = true;
 }
